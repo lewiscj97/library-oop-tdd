a5b9f7a0cd148fa555a3592409fa7376f05bbe3f
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-28 12:23:32 +0100

    feat(book): add ability to mark a book as damaged and check its state

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,15 @@
 class Book
   attr_reader :title
   attr_reader :author
+  attr_writer :damaged
   
   def initialize(title, author)
     @title = title
     @author = author
+    @damaged = false
+  end
+
+  def damaged?
+    @damaged
   end
 end
