3235e7694ebd26efb6e25ddefc7b280da5b6d79d
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-28 12:11:02 +0100

    feat(*): add ability to list the books in an easy to read way

--- lib/library.rb
+++ lib/library.rb
@@ -8,4 +8,12 @@ class Library
   def add_book(book)
     @books << book
   end
+
+  def all_books
+    hash_list = []
+    @books.each do |book|
+      hash_list << {title: book.title, author: book.author}
+    end
+    hash_list
+  end
 end
