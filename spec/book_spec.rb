b711dd10740bf01c985f7f5e12a5f355122da5ee
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-28 12:25:02 +0100

    refactor(book): refactor tests to remove unused tests and DRY code

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,31 +1,24 @@
 require 'book'
 
 describe Book do
-  describe "#initialize" do
-    it "adds a title and an author property" do
-      Book.new("This is the Title", "Lewis Jones")
-    end
-  end
-
+  before(:each) { @book = Book.new("This is the Title", "Lewis Jones") }
+  
   describe "#title" do
     it "returns the title of the book" do
-      book = Book.new("This is the Title", "Lewis Jones")
-      expect(book.title).to eq "This is the Title"
+      expect(@book.title).to eq "This is the Title"
     end
   end
 
   describe "#author" do
     it "returns the author of the book" do
-      book = Book.new("This is the Title", "Lewis Jones")
-      expect(book.author).to eq "Lewis Jones"
+      expect(@book.author).to eq "Lewis Jones"
     end
   end
 
   describe "#damaged" do
     it "sets whether the book is damaged or not" do
-      book = Book.new("This is the Title", "Lewis Jones")
-      book.damaged = true
-      expect(book.damaged?).to eq true
+      @book.damaged = true
+      expect(@book.damaged?).to eq true
     end
   end
 end
