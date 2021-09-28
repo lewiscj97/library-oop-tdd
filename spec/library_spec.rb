db924c1b1be58eebb6f428e4730ec0aca1debe59
Lewis Jones <lewis.jones1997@protonmail.com>
2021-09-28 12:08:24 +0100

    test(library): test showing all books as a hash

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,12 +1,24 @@
 require 'library'
 
 describe Library do
-  let(:book) { double :book }
+  let(:book) { double :book, title: "This is the Title", author: "Lewis"}
 
   describe "#add_book" do
     it "adds a book to the collection of books" do
       subject.add_book(book)
       expect(subject.books).to include book
+    end
+  end
+
+  describe "#all_books" do
+    it "lists all the books in the library" do
+      subject.add_book(book)
+      subject.add_book(book)
+
+      expect(subject.all_books).to eq [
+        {title: "This is the Title", author: "Lewis"},
+        {title: "This is the Title", author: "Lewis"}
+      ]
     end
   end
 end
